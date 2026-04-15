7a3ed9eebe71ded76a31a71d623d33ee8b5f0688
White Hawk <ars0001orlov@gmail.com>
2026-04-15 16:51:36 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -7,5 +7,16 @@ arr.forEach((item) => {
     skills.appendChild(card)
 })
 
+document.getElementById('copyMail').addEventListener('click', async () => {
+  const text = document.getElementById('copyMail').textContent;
+
+  try {
+    await navigator.clipboard.writeText(text);
+    alert('Текст скопирован в буфер обмена!');
+  } catch (err) {
+    console.error('Ошибка при копировании: ', err);
+    alert('Не удалось скопировать текст');
+  }
+});
 
 
